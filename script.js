455ff5dd903090e9b96a8572ee449fc7b31e9ec1
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2023-11-10 12:04:59 +0530

    All the file uploaded

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed=new Typed(".name",{
+    strings:["Web Development","HTML","CSS","JavaScript","pHp","My Sql","React JS","Frame Works"],
+    typedSpeed:90,
+    backSpeed:85,
+    loop:true,
+})
+
+let log=document.getElementById("login")
+
+log.addEventListener=()=>{
+    alert("Successfully Login..")
+}
